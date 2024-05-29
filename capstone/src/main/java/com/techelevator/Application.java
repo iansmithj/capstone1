4d0e6a866c4a4f3512ac265c8e7b27a73bf156af
mike levy <michael.levy@techelevator.com>
2024-05-29 14:30:56 -0400

    seed repo

---
+++ capstone/src/main/java/com/techelevator/Application.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Application application = new Application();
+		application.run();
+	}
+
+	public void run() {
+		// start your code here
+	}
+}
