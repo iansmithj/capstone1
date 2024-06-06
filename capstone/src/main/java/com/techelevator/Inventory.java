ba71e9b68138c739873c3ce50ec12cb8bfb32d53
Ian Smith <smithjian22@gmail.com>
2024-06-06 11:19:16 -0400

    fd

--- capstone/src/main/java/com/techelevator/Inventory.java
+++ capstone/src/main/java/com/techelevator/Inventory.java
@@ -2,61 +2,10 @@ package com.techelevator;
 
 import java.io.File;
 
-public class Inventory {
-
- private double price;
-
- private String name;
-
- private String code;
-
- private String itemCategory;
-
- private String pipe1;
-
- private String pipe2;
-
- private String pipe3;
+public class Stock {
 
 
 
-
-
- public Inventory(String code, String pipe1, String name, String pipe2, double price, String pipe3, String itemCategory) {
-this.price = price;
-this.name = name;
-this.code = code;
-this.itemCategory = itemCategory;
-this.pipe1 = pipe1;
-  this.pipe2 = pipe2;
-  this.pipe3 = pipe3;
- }
-
- public double getPrice() {
-return price;
- }
-
- public String getName() {
-  return name;
- }
-
- public String getCode() {
-  return code;
- }
-
- public String getItemCategory() {
-  return itemCategory;
- }
-
- public String getPipe1() {
-  return pipe1;
- }
- public String getPipe2() {
-  return pipe2;
- }
- public String getPipe3() {
-  return pipe3;
- }
 
 
 
--- capstone/src/main/java/com/techelevator/MainMenu.java
+++ capstone/src/main/java/com/techelevator/MainMenu.java
@@ -1,8 +1,17 @@
 package com.techelevator;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class MainMenu extends Application{
+
+Scanner scanner = new Scanner(System.in);
+
+    private Scanner userInput;
+
+    System.out.println("enter numbers 1-4");
+    String menuOptions = userInput.nextLine();
 
 
 
